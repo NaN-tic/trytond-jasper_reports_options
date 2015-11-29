ad0b6fd1c857bbc68fc8013d6b1931da8f9fb1d1
resteve <resteve@zikzakmedia.com>
2015-11-29 22:29:28 +0100

    Tests

--- tests/test_jasper_reports_options.py
+++ tests/test_jasper_reports_options.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
-# This file is part jasper_reports_options module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the jasper_reports_options module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
-    test_depends
-from trytond.transaction import Transaction
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class JasperReportsOptionsTestCase(unittest.TestCase):
+class JasperReportsOptionsTestCase(ModuleTestCase):
     'Test Jasper Reports Options module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('jasper_reports_options')
-
-    def test0005views(self):
-        'Test views'
-        test_view('jasper_reports_options')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'jasper_reports_options'
 
 
 def suite():
@@ -29,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         JasperReportsOptionsTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
